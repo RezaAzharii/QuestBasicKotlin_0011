d503d739b779eb33627cc49a2c70b247ee2b4074
RezaAzharii <rezaazhari867@gmail.com>
2024-10-10 16:16:12 +0700

    menambahkan fungi equals.

--- app/src/main/java/com/example/pertemuan1/DataClass.kt
+++ app/src/main/java/com/example/pertemuan1/DataClass.kt
@@ -5,3 +5,13 @@ data class DataClass(
     var email: String
 )
 
+fun main(){
+    val data = DataClass(1, "indra@gmail.com")
+    println(data)
+
+    //Fungsi Equals
+    val data2 = DataClass(1, "makan@gmail.com")
+    println(data == data2)
+
+
+}
