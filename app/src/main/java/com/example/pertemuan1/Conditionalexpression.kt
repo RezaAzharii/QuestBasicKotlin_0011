a264934e5361d45c814473510f41c7061bcbd6c6
RezaAzharii <rezaazhari867@gmail.com>
2024-10-10 18:26:18 +0700

    membuat perulangan dengan variable ListOf

--- app/src/main/java/com/example/pertemuan1/Conditionalexpression.kt
+++ app/src/main/java/com/example/pertemuan1/Conditionalexpression.kt
@@ -42,7 +42,12 @@ fun Perulangan(){
     println("==== Perulangan ====")
 
     for (i in 1..5){
-        println("Perulngan ke-$i")
+        println("Perulangan ke-$i")
+    }
+
+    val cars = listOf("BMW", "Toyota", "Honda", "Audi")
+    for (car in cars){
+        println("Car type: $car")
     }
 }
 
