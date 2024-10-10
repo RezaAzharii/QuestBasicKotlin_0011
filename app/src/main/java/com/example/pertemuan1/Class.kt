42b0a4b38edcd0135fb4800a2fb8651192b6b1e1
RezaAzharii <rezaazhari867@gmail.com>
2024-10-10 16:36:08 +0700

    menambahkan isi email baru dengan email "jane@gmail.com"

--- app/src/main/java/com/example/pertemuan1/Class.kt
+++ app/src/main/java/com/example/pertemuan1/Class.kt
@@ -8,4 +8,10 @@ fun main(){
     val contact = Contact(1, "mary@gmail.com")
     //Prints the value of the property: email
     println(contact.email)
+
+    //Updates the value of the property: email
+    contact.email = "jane@gmail.com"
+
+    //Prints the new value of the property: email
+    println(contact.email)
 }
