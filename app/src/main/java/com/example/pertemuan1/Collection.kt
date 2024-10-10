585419c3fdbcccfae9324af2938b89aae349cd01
RezaAzharii <rezaazhari867@gmail.com>
2024-10-10 16:45:42 +0700

    Membuat function list Read-Only

---
+++ app/src/main/java/com/example/pertemuan1/Collection.kt
@@ -0,0 +1,14 @@
+package com.example.pertemuan1
+
+//List adalah Kumpulan data yang disusun secara berurutan dari
+//List bersifat ordered, artinya data yang dimasukan akan memiliki posisi sesuai urutan
+//List bersifat mutable dan read only, artinya data yang dimasukan dapat diubah dan tidak dapat diubah
+//List Read-Only menggunakan listof
+//List Mutable menggunakan mutablelistof
+
+fun main(){
+    println("=== List ===")
+    //List Read-Only
+    val readOnlyAbjad = listOf("A", "B", "C")
+    println(readOnlyAbjad)
+}
